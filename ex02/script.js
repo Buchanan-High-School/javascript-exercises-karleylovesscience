06b2d5666124c951d39a5552b9aaa5a442eda892
karleylovesscience <williamskarley114@gmail.com>
2024-11-13 12:18:55 -0500

    task 1 and task 2

--- ex02/script.js
+++ ex02/script.js
@@ -1,11 +1,18 @@
 // Write your Task 1 code inside this function!
 function task1() {
-  console.log("Running task 1");
+  console.log("Running task 10");
+  //1. target the div
+  let target=document.querySelector("#text-content")
+  console.log(target)
+  //2.set inner text to something new 
+  target.innerText="something new"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+let target=document.querySelector("#task2")
+  target.style.backgroundColor="yellow"
 }
 
 // Write your task 3 code inside this function!
